29d50f28dce822844776af43e7668475c9e9f2d5
nmacchitella <nicola.macchitella@gmail.com>
2022-05-31 10:57:54 +0200

    password

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -7,5 +7,6 @@ SECRET_KEY = os.environ.get('PASSWORD', False)
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
-c.NotebookApp.password=SECRET_KEY
-c.NotebookApp.token=""
+c.NotebookApp.password="argon2:$argon2id$v=19$m=10240,t=10,p=8$ShJ/e/jKfyxoIGq8IOtEAw$P5os6s8T5TlqsvHvPBQK4czCeRujwJw19x1/hyfyIiQ"
+print(SECRET_KEY)
+#c.NotebookApp.token=""
