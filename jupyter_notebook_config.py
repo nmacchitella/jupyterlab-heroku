9ff9f3fa7580fa18c018558bf4bda4c3b3ee38e7
nmacchitella <nicola.macchitella@gmail.com>
2022-05-31 10:15:06 +0200

    password

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,8 +1,10 @@
 # https://devcenter.heroku.com/articles/ssl-endpoint
 #
+SECRET_KEY = os.environ.get('PASSWORD', False)
+
 # As long as the notebook lives in the herokuapp.com domain,
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
-c.NotebookApp.password="sha1:bca9422a4bbf:6b2c319684b3b95d30245b6b3b9ee307245032bb"
+c.NotebookApp.password=SECRET_KEY 
 c.NotebookApp.token=""
